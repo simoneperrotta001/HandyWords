e3260b8890300a36e2ae0f0aab6a6cd41a2d4b07
Simone <simone.perrotta001@studenti.uniparthenope.it>
2023-03-28 09:47:35 +0200

    Initial Commit

---
+++ HandyWords/HandyWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandyWordsApp.swift
+//  HandyWords
+//
+//  Created by Simone Perrotta on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct HandyWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
